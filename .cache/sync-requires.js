758305d5fbdcc1477466db6b6fe0a58ce22d41c9
Filip Franicevic <ffrani@Filips-MacBook-Air.local>
2025-05-18 13:46:36 +0200

    Updates

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Filip\\Desktop\\filip-henon-wedding-invitation-croatia\\.cache\\dev-404-page.js"))),
+  "component---src-pages-e-ticket-js": hot(preferDefault(require("C:\\Users\\Filip\\Desktop\\filip-henon-wedding-invitation-croatia\\src\\pages\\e-ticket.js"))),
+  "component---src-pages-generate-link-js": hot(preferDefault(require("C:\\Users\\Filip\\Desktop\\filip-henon-wedding-invitation-croatia\\src\\pages\\generate-link.js"))),
+  "component---src-pages-generate-qr-js": hot(preferDefault(require("C:\\Users\\Filip\\Desktop\\filip-henon-wedding-invitation-croatia\\src\\pages\\generate-qr.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Filip\\Desktop\\filip-henon-wedding-invitation-croatia\\src\\pages\\index.js"))),
+  "component---src-pages-send-wishes-js": hot(preferDefault(require("C:\\Users\\Filip\\Desktop\\filip-henon-wedding-invitation-croatia\\src\\pages\\send-wishes.js")))
+}
+
